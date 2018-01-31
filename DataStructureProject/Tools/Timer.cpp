66ff30748cb0d8c0edc7b6096e77bb4457d1d120
LokiTheZorua <31518758+LokiTheZorua@users.noreply.github.com>
2018-01-31 11:59:04 -0700

    made the Timer controller file and added in all the methods

--- DataStructureProject/Tools/Timer.cpp
+++ DataStructureProject/Tools/Timer.cpp
@@ -10,4 +10,34 @@
 
 using namespace std;
 
+Timer :: Timer()
+{
+    executionTime = 0;
+}
 
+void Timer :: resetTimer()
+{
+    executionTime = 0;
+}
+
+void Timer :: startTimer()
+{
+    executionTime = clock();
+}
+
+void Timer :: stopTimer()
+{
+    assert(executionTime != 0);
+    executionTime = clock() - executionTime;
+}
+
+void Timer :: displayInformation()
+{
+    cout << "The execution time is: " << executionTime << endl;
+    cout << "In human time it is " << double (executionTime)/CLOCKS_PER_SEC;
+}
+
+long Timer :: getTimeInMicroseconds()
+{
+    return executionTime;
+}
