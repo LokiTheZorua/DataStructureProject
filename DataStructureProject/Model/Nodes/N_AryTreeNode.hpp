ec9f3df2e5819546eb9b7a8f860eec2cf3587380
LokiTheZorua <31518758+LokiTheZorua@users.noreply.github.com>
2018-04-27 12:20:06 -0600

    made HashNode

---
+++ DataStructureProject/Model/Nodes/N_AryTreeNode.hpp
@@ -0,0 +1,86 @@
+//
+//  N_AryTreeNode.hpp
+//  DataStructureProject
+//
+//  Created by Jones, Alec on 4/23/18.
+//  Copyright © 2018 Jones, Alec. All rights reserved.
+//
+
+#ifndef N_AryTreeNode_h
+#define N_AryTreeNode_h
+
+using namespace std;
+
+template <class Type>
+class N_AryTreeNode : public Node<Type>
+{
+private:
+    vector<N_AryTreeNode<Type>*> nodes;
+    N_AryTreeNode<Type> * root;
+    
+public:
+    N_AryTreeNode<Type>();
+    ~N_AryTreeNode<Type>();
+    N_AryTreeNode<Type>(Type data);
+    
+    vector<N_AryTreeNode<Type>*> getNodes();
+    N_AryTreeNode<Type> * getRoot();
+    int addChildCount();
+    
+    void setRoot(N_AryTreeNode<Type> * root);
+    void addChild(Type data);
+}
+
+template <class Type>
+N_AryTreeNode<Type> :: N_AryTreeNode()
+{
+    //unsed
+}
+
+template <class Type>
+N_AryTreeNode<Type> :: N_AryTreeNode(Type data) : Node<Type>(data)
+{
+    
+}
+
+template <class Type>
+N_AryTreeNode<Type> :: ~N_AryTreeNode()
+{
+    for (int index = nodes.size() - 1; index >= 0; index --)
+    {
+        delete nodes[index];
+    }
+}
+
+template <class Type>
+N_AryTreeNode<Type> * N_AryTreeNode<Type> :: getRoot()
+{
+    return root;
+}
+
+template <class Type>
+vector<N_AryTreeNode<Type>*> N_AryTreeNOde<Type> :: getNodes()
+{
+    return nodes;
+}
+
+template <class Type>
+int N_AryTreeNode<Type> :: getChildCount()
+{
+    return nodes.size()
+}
+
+template <class Type>
+void N_AryTreeNode<Type> :: setRoot(N_AryTreeNode<Type> * root)
+{
+    this->root = root;
+}
+
+template <class Type>
+void N_AryTreeNode<Type> :: addChild(Type data)
+{
+    N_AryTreeNode<Type> * node = new N_AryTreeNode<Type>(data);
+    nodes.push_back(node);
+}
+
+#endif /* N_AryTreeNode_h */
